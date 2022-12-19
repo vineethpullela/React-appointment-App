3c46b6914036c149cec50d48bfe846cf2cbe6e6a
vineeth <15072cm031@gmail.com>
2022-12-19 16:19:36 +0000

    appointment app

--- src/components/Appointments/index.js
+++ src/components/Appointments/index.js
@@ -28,6 +28,27 @@ class Appointments extends Component {
             />
           </div>
           <hr className="line" />
+          <div className="appointments-container">
+            <h1 className="appointments-heading">Appointments</h1>
+            <button type="button" className="starred-button">
+              Starred
+            </button>
+          </div>
+          <div className="appointments-list-container">
+            <div className="appointment-list-item">
+              <div className="appointment-name-container">
+                <p className="appointment-name">Dentist</p>
+                <button type="button" className="starred-item-button">
+                  <img
+                    src="https://assets.ccbp.in/frontend/react-js/appointments-app/star-img.png"
+                    className="star-img"
+                    alt="star"
+                  />
+                </button>
+              </div>
+              <p className="appointment-date">Date: 20 july 2021, Tuesday</p>
+            </div>
+          </div>
         </div>
       </div>
     )
